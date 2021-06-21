7cbd037b22b7303db824ef07a2d82339f25f3b72
TGV <tgv1975@yahoo.com>
2021-06-21 11:41:36 +0300

    Fix coverage path

--- projects/dragdrop/karma.conf.js
+++ projects/dragdrop/karma.conf.js
@@ -16,8 +16,9 @@ module.exports = function (config) {
         client: {
             clearContext: false // leave Jasmine Spec Runner output visible in browser
         },
-        coverageIstanbulReporter: {
+        coverageReporter: {
             dir: require('path').join(__dirname, '../../coverage'),
+            subdir: '.',
             reports: ['html', 'lcovonly'],
             fixWebpackSourcePaths: true
         },
